f91d721bde8f3420be1353ce0edd59895c7e6b40
Manogari Kumaraguru <manogarikumaraguru@gmail.com>
2025-04-29 23:27:34 -0400

    css

---
+++ ReactFashion/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Nav from '../components/Nav';
+export default function Header() { 
+    return(
+    <>
+        <h1>Sartre's List</h1>
+        <h2>Better-Dressed People</h2>
+        <Nav/>
+    </>
+    )
+}
---
+++ ReactFashion/src/components/Nav.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+export default function Nav()
+{
+    <nav aria-label="Main Navigation" role="navigation">
+            <ul className="navi">
+                
+                <li><a href="...">Women's</a></li>
+                <li><a href="...">Men's</a></li>
+                <li><a href="...">On the Street</a></li>
+                <li><a href="...">The catwalk</a></li>
+                <li><a href="...">AdWatch</a></li>
+                <li><a href="...">About</a></li>
+               
+            </ul>
+        </nav>
+}
